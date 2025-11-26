b2c3104cd1ddec53d7e0ef4d251c2391ea0ddd0c
Ben V <veliebm@gmail.com>
2025-11-25 21:47:02 -0800

    feat: copy benvelie.com's contents to this app

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,14 @@
 """A simple Flask app for serving my website, http://benvelie.com"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello from Flask!"
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
